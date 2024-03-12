b5e40b6ea57f1c56b88468db2450487574e976c2
JosephK1357 <21558994-JosephK1357@users.noreply.replit.com>
2024-03-12 20:20:15 +0000

    hello world added

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ database.create_table("houses", "name", "price", "size")
 database.add_entries("houses", {"name":"Bob's house", "price":"1500 dollars", "size":"5 square feet"}, {"name": "Jim's cottage", "price":"6 dollars"})
 print(database.fetch_all_entries("houses"))
 
+print("helloworld")
